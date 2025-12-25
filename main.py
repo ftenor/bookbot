dbffc98c1c24d42a6392629ebe49abc9564b7dfd
ftenor <felix.tenor96@gmail.com>
2025-12-25 12:04:09 +0100

    now takes a file path as argument instead of hardcode

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from stats import count_words, count_chars, get_sorted_list
 import sys
+from stats import count_words, count_chars, get_sorted_list
 
 def get_book_text(file_path):
     file_contents = file_path.read()
@@ -13,7 +13,11 @@ def print_char_count(char_count):
 
 
 def main():
-    file_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
     with open(file_path) as fp:
         text = get_book_text(fp)
         sorted_list = get_sorted_list(count_chars(text))
@@ -25,7 +29,6 @@ def main():
         print("--------- Character Count -------")
         print_char_count(sorted_list)
         print("============= END ===============")
-        
 
 main()
 
